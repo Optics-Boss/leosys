1a2e03a5c741fb6975b58863c44aa37f1e3d0392
Optics-Boss <->
2024-05-14 21:01:32 +0200

    Started with memory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::env;
+use sysinfo::{
+    Components, Disks, Networks, System,
+};
 
 fn main() {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
     println!("OS: {} {}", env::consts::OS, env::consts::ARCH);
-    for (key, value) in env::vars() {
-        println!("{key:?}: {value:?}");
-    }
+    println!("Memory: {}MiB / {}MiB", 
+             (sys.used_memory() / 100000),
+             (sys.total_memory() / 100000), 
+     );
 }
